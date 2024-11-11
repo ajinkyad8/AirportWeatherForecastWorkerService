e76a4d55dbefc96afd9d80dbbb1280248fc61599
ajinkya <ajinkyad08@gmail.com>
2024-11-11 07:03:19 +0530

    Added catch block.

--- AirportWeatherForecastWorkerService/Worker.cs
+++ AirportWeatherForecastWorkerService/Worker.cs
@@ -8,15 +8,18 @@ namespace AirportWeatherForecastWorkerService
         private readonly ITAFProvider _tAFProvider;
         private readonly TAFStorageProvider _tAFStorageProvider;
         private readonly IAirportProvider _airportProvider;
+        private readonly ILogger<Worker> _logger;
         private readonly int _frequency;
 
         public Worker(ITAFProvider tAFProvider, TAFStorageProvider tAFStorageProvider,
              IAirportProvider airportProvider,
-             IConfiguration configuration)
+             IConfiguration configuration,
+             ILogger<Worker> logger)
         {
             _tAFProvider = tAFProvider;
             _tAFStorageProvider = tAFStorageProvider;
             _airportProvider = airportProvider;
+            _logger = logger;
             _frequency = configuration.GetValue<int>("Frequency");
         }
 
@@ -24,11 +27,18 @@ namespace AirportWeatherForecastWorkerService
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                var airports = await _airportProvider.GetAirports();
-                var tafData = await _tAFProvider.GetTAFs(airports);
-                var storageProvider = _tAFStorageProvider.GetTAFStorage();
-                await storageProvider.StoreTAFs(tafData);
-                await Task.Delay(_frequency, stoppingToken);
+                try
+                {
+                    var airports = await _airportProvider.GetAirports();
+                    var tafData = await _tAFProvider.GetTAFs(airports);
+                    var storageProvider = _tAFStorageProvider.GetTAFStorage();
+                    await storageProvider.StoreTAFs(tafData);
+                    await Task.Delay(_frequency, stoppingToken);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"Error while trying to save TAF data. Message: {ex.Message}. Stacktrace: {ex.StackTrace}.");
+                }
             }
         }
     }
